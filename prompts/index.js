6636fb410d2539987f77bff1c2abe7f1941dab7b
Optical23 <aaroneferguson@gmail.com>
2022-03-10 19:14:15 -0600

    latest

--- prompts/index.js
+++ prompts/index.js
@@ -1,3 +1,78 @@
+const startPrompt = [{
+    type: 'list',
+    name: 'action',
+    message: 'What action would you like to make?',
+    choices: [
+        'View departments',
+        'View roles',
+        'View employees',
+        'Add department',
+        'Add role',
+        'Add employee',
+        'Change employee role',
+        'Exit'
+    ]
+}];
 
-const prompts = '';
-module.exports = prompts;
+const addDepartmentPrompt = [{
+    type: 'input',
+    message: 'What is the name of the department?',
+    name: 'departmentName',
+}];
+
+const addRolePrompt = [
+    {
+        type: 'input',
+        message: 'What is the name of the role?',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'What is the salary of the role?',
+        name: 'salary'
+    },
+    {
+        type: 'input',
+        message: 'Which department does the role belong to?',
+        name: 'department'
+    }
+];
+
+const addEmployeePrompt = [
+    {
+        type: 'input',
+        message: "What is the employee's first name?",
+        name: 'firstName'
+    },
+    {
+        type: 'input',
+        message: "What is the employee's last name?",
+        name: 'lastName'
+    },
+    {
+        type: 'input',
+        message: "What is the employee's role?",
+        name: 'role'
+    },
+    {
+        type: 'input',
+        message: "Who is the employee's manager?",
+        name: 'manager'
+    }
+];
+
+const changeEmployeeRolePrompt = [
+    {
+        type: 'list',
+        message: "Enter the Employee's ID to update the role",
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: "What is the employee's new role?",
+        name: 'newRole'
+    }
+];
+
+const Prompts = {startPrompt,addDepartmentPrompt,addRolePrompt,addEmployeePrompt,changeEmployeeRolePrompt};
+module.exports = Prompts;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const express = require('express');
 require('console.table');
+const {startPrompt,addDepartmentPrompt,addRolePrompt,addEmployeePrompt,changeEmployeeRolePrompt} = require('./prompts');
 
 
 const PORT = process.env.PORT || 3001;
@@ -11,27 +12,79 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'test',
-        database: 'employee_vision'
+        password: 'PlmPlm23!',
+        database: 'employee_db'
     },
     console.log('Database server is running')
 );
 
-db.connect(err =>{
-    if (err) { return console.log(err);};
-    console.table('Database of Employees is live');
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database of Employees is live');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
     promptStart();
 });
 
 const promptStart = () => {
-    inquirer.prompt([
-        {
-            type: 'list',
-            name: 'menu',
-            message: 'Select an action for the employee tracker.',
-            choices: [
-                'V'
-            ]
+    inquirer.prompt([...startPrompt]).then(response => {
+        switch(response.action){
+            case 'View departments':
+                viewDepartments();
+                break;
+            case 'View roles':
+                viewRoles();
+                break;
+            case 'View employees':
+                viewEmployees();
+                break;
+            case 'Add departments':
+                addDepartment();
+                break;
+            case 'Add role':
+                addRole();
+                break;
+            case 'Add employee':
+                addEmployee();
+                break;
+            case 'Change employee role':
+                changeEmployeeRole();
+                break;
+            case 'Exit':
+                exit();
+                break;
+            default:
+                exit();
         }
-    ])
+    });
+}
+
+
+const viewDepartments = () => {
+    db.query('SELECT * FROM department', function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        promptStart();
+    });
+};
+
+const viewRoles = () => {
+    db.query('SELECT * FROM role', function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        promptStart();
+    });
+};
+
+const viewEmployees = () => {
+    db.query('SELECT * FROM employee', function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        promptStart();
+    });
+};
+
+const addDepartment = () => {
+    inquirer.prompt([...addDepartmentPrompt]);
 }
